9407334edfd840d33aa1394e3590f397ac1b67cb
Wesley Ford <wes@wesdev.pro>
2023-10-09 17:31:55 -0500

    Fix Rollup Error And Bump to `v0.0.4`

--- packages/icks/configs/rollup.config.js
+++ packages/icks/configs/rollup.config.js
@@ -1,30 +1,28 @@
 // rollup.config.js
-import typescript from "@rollup/plugin-typescript";
+const typescript = require("@rollup/plugin-typescript");
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: "lib/index.mjs",
-        format: "es",
-        name: "icks",
-        sourcemap: false,
-        exports: "named",
-      },
-      {
-        file: "lib/index.umd.js",
-        name: "icks",
-        format: "umd",
-        sourcemap: false,
-        exports: "named",
-      },
-    ],
-    plugins: [
-      typescript({
-        tsconfig: "./configs/tsconfig.esm.json",
-        sourceMap: false,
-      }),
-    ],
-  },
-];
+module.exports = {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "lib/index.mjs",
+      format: "es",
+      name: "icks",
+      sourcemap: false,
+      exports: "named",
+    },
+    {
+      file: "lib/index.umd.js",
+      name: "icks",
+      format: "umd",
+      sourcemap: false,
+      exports: "named",
+    },
+  ],
+  plugins: [
+    typescript({
+      tsconfig: "./configs/tsconfig.esm.json",
+      sourceMap: false,
+    }),
+  ],
+};
