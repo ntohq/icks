d5afc0ef0ef494673500fb1a5943db704b1fa134
Wesley Ford <wes@wesdev.pro>
2023-10-09 16:41:48 -0500

    Delete packages/icks/lib directory

--- packages/icks/lib/externals.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './library/Math';
-export * from './library/Vector';
--- packages/icks/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import * as icks from './externals';
-export * from './externals';
-export { icks };
-export default icks;
