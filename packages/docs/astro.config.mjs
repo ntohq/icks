f47c28e516fe2ea31beae00cac53f6755e8e4caa
Wesley Ford <wes@wesdevpro.com>
2023-11-15 16:27:54 -0600

    chore(docs): migrate to `astro` + `starlight`

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,9 +1,27 @@
 import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
-
-import tailwind from "@astrojs/tailwind";
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
 });
